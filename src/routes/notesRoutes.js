18eb705fdf948b1a00bbeb4eeba21b06baa017d8
Yasmin <jana.afonina@gmail.com>
2025-10-13 09:27:53 +0300

    new code notesRoutes.js&notesValidation.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,52 +1,53 @@
+
 import { Router } from 'express';
-import { celebrate, Segments } from 'celebrate'; 
+import { celebrate, Segments } from 'celebrate';
 import {
-  getAllNotes,
-  getNoteById,
-  createNote,
-  updateNote,
-  deleteNote,
+    getAllNotes,
+    getNoteById,
+    createNote,
+    updateNote,
+    deleteNote,
 } from '../controllers/notesController.js';
 import {
-  getAllNotesSchema,
-  noteIdSchema,
-  createNoteSchema,
-  updateNoteSchema,
+    getAllNotesSchema,
+    noteIdSchema,
+    createNoteSchema,
+    updateNoteBodySchema, 
 } from '../validations/notesValidation.js'; 
 
 const router = Router();
 
 router.get(
-  '/notes', 
-  celebrate({ [Segments.QUERY]: getAllNotesSchema }), 
-  getAllNotes,
+    '/notes',
+    celebrate({ [Segments.QUERY]: getAllNotesSchema }), 
+    getAllNotes,
 );
 
 router.post(
-  '/notes', 
-  celebrate({ [Segments.BODY]: createNoteSchema }), 
-  createNote,
+    '/notes',
+    celebrate({ [Segments.BODY]: createNoteSchema }), 
+    createNote,
 );
 
 router.get(
-  '/notes/:noteId', 
-  celebrate({ [Segments.PARAMS]: noteIdSchema }), 
-  getNoteById,
+    '/notes/:noteId',
+    celebrate({ [Segments.PARAMS]: noteIdSchema }), 
+    getNoteById,
 );
 
 router.patch(
-  '/notes/:noteId', 
-  celebrate({
-    [Segments.PARAMS]: noteIdSchema, 
-    [Segments.BODY]: updateNoteSchema, 
-  }),
-  updateNote,
+    '/notes/:noteId',
+    celebrate({
+        [Segments.PARAMS]: noteIdSchema, 
+        [Segments.BODY]: updateNoteBodySchema, 
+    }),
+    updateNote,
 );
 
 router.delete(
-  '/notes/:noteId', 
-  celebrate({ [Segments.PARAMS]: noteIdSchema }), 
-  deleteNote,
+    '/notes/:noteId',
+    celebrate({ [Segments.PARAMS]: noteIdSchema }), 
+    deleteNote,
 );
 
 export default router;
