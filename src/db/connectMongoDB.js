307f42ac6be28a400fbcb4874e07c4e9d09af9ae
Yasmin <jana.afonina@gmail.com>
2025-10-22 20:54:33 +0300

    new connectMongoDB

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,13 +1,24 @@
 import mongoose from 'mongoose';
-import Note from '../models/note.js';
+import { Note } from '../models/note.js'; 
+import { User } from '../models/user.js'; 
+import { Session } from '../models/session.js'; 
 
 export const connectMongoDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log('✅ MongoDB connection established successfully');
-        await Note.syncIndexes();
-    } catch (error) {
-        console.error('❌ Failed to connect to MongoDB:', error.message);
-        process.exit(1);
-    }
+  if (!process.env.MONGO_URL) {
+    console.error('❌ MONGO_URL is not defined in the environment variables!');
+    process.exit(1);
+  }
+  
+  try {
+   
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('✅ MongoDB connection established successfully');
+    await Note.syncIndexes();
+    await User.syncIndexes(); 
+    await Session.syncIndexes(); 
+    
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB:', error.message);
+    process.exit(1);
+  }
 };
