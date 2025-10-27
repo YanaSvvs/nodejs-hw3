798a0a25b4f509ce31c6d166b723c5d7d01b4fe7
Yasmin <jana.afonina@gmail.com>
2025-10-27 13:39:26 +0200

    Fix: Added missing loginUserSchema and registerUserSchema to validation file

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,6 +1,20 @@
 
 import Joi from 'joi';
 
+const userCredentialsSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.email': 'Email must be a valid email address.',
+    'any.required': 'Email is required.',
+  }),
+  password: Joi.string().required().messages({
+    'any.required': 'Password is required.',
+  }),
+});
+
+export const registerUserSchema = userCredentialsSchema.clone();
+
+export const loginUserSchema = userCredentialsSchema.clone();
+
 export const requestResetEmailSchema = Joi.object({
   email: Joi.string().email().required().messages({
     'string.email': 'Email must be a valid email address.',
